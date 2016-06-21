1310fd87c3e11b0965fba2f818dd7f26c231305f
AxesOfEvil <fmy4caf7cg@snkmail.com>
2016-06-20 18:07:45 -0700

    Sync 'audio' applications with LE 7.0

--- packages/audio/dcadec/package.mk
+++ packages/audio/dcadec/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="dcadec"
-PKG_VERSION="37d8e68"
+PKG_VERSION="0e07438"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/foo86/dcadec"
-PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="https://github.com/foo86/dcadec/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="audio"
--- packages/audio/speex/package.mk
+++ packages/audio/speex/package.mk
@@ -16,18 +16,19 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="libogg"
-PKG_VERSION="1.3.2"
+PKG_NAME="speex"
+PKG_VERSION="1.2rc2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
-PKG_SITE="https://www.xiph.org/ogg/"
-PKG_URL="http://downloads.xiph.org/releases/ogg/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SITE="http://downloads.us.xiph.org/releases/speex"
+#PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="$PKG_SITE/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="audio"
-PKG_SHORTDESC="libogg: Open source bitstream container format"
-PKG_LONGDESC="Libogg contains necessary functionality to create, decode, and work with Ogg bitstreams."
+PKG_SHORTDESC="Speex / OPUS audio codec"
+PKG_LONGDESC="Speex / OPUS audio codec"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
