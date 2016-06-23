14f54d7dbe30b2767a2a64d6ed6fca1b0522d0e6
AxesOfEvil <fmy4caf7cg@snkmail.com>
2016-06-22 20:47:32 -0700

    Sync LE7 build environment and systemd and bluez

--- config/noobs/partition_setup.sh
+++ config/noobs/partition_setup.sh
@@ -1,39 +1,37 @@
 #!/bin/sh -x
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2013 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 LibreELEC Team
 #
-#  This Program is free software; you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2, or (at your option)
-#  any later version.
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
 #
-#  This Program is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.tv; see the file COPYING.  If not, write to
-#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
-#  http://www.gnu.org/copyleft/gpl.html
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-MOUNTPOINT="/tmp/OpenELEC-System"
+MOUNTPOINT="/tmp/LibreELEC-System"
 
 md5sumCheck() {
   ( cd $MOUNTPOINT
     echo "checking MD5: $1"
     md5sum -c $1.md5
     if [ "$?" = "1" ]; then
-      echo "######################################################"
-      echo "#                                                    #"
-      echo "# OpenELEC failed md5 check - Installation will quit #"
-      echo "#                                                    #"
-      echo "#    Your original download was probably corrupt.    #"
-      echo "#  Please visit www.openelec.tv and get another copy #"
-      echo "#                                                    #"
-      echo "######################################################"
+      echo "#######################################################"
+      echo "#                                                     #"
+      echo "#     Invalid MD5 checksum - Installation stopped     #"
+      echo "#                                                     #"
+      echo "#         Your download was probably corrupt.         #"
+      echo "#    Visit https://libreelec.tv to download again     #"
+      echo "#                                                     #"
+      echo "#######################################################"
       exit 1
     fi
     rm -rf $1.md5
